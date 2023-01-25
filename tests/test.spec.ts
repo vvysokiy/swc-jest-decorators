9e8eced8050e4046addaca80820bd9503f37c93b
Vladimir Vysokiy <vladimir.vysokij@yandex.ru>
2023-01-25 11:01:54 +0400

    test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,7 +1,7 @@
-// import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
-// @Injectable()
-// export class Test {}
+@Injectable()
+export class Test {}
 
 describe('describe', () => {
   test('test', () => {
